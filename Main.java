d812a674b635f6eba8d905c3378971237f21e8fd
Andrey <cliff_06@bk.ru>
2024-01-31 22:12:42 +0500

    создали код в файле Main

--- Main.java
+++ Main.java
@@ -1,3 +1,45 @@
+
+
+// Малинин Андрей
+
 public class Main {
-    
+    public static void main(String[] args) {
+        // Создаем несколько товаров
+        Product product1 = new Product("Товар1", 100.0, 5);
+        Product product2 = new Product("Товар2", 200.0, 4);
+        Product product3 = new Product("Товар3", 150.0, 3);
+
+        // Создаем категории и добавляем товары
+        ProductCategory category1 = new ProductCategory("Категория1");
+        category1.addProduct(product1);
+        category1.addProduct(product2);
+
+        ProductCategory category2 = new ProductCategory("Категория2");
+        category2.addProduct(product2);
+        category2.addProduct(product3);
+
+        // Выводим на консоль каталог продуктов
+        System.out.println("Каталог продуктов:");
+        System.out.println(category1.getName() + ":");
+        for (Product product : category1.getProducts()) {
+            System.out.println(product);
+        }
+        System.out.println(category2.getName() + ":");
+        for (Product product : category2.getProducts()) {
+            System.out.println(product);
+        }
+
+        // Создаем пользователей и добавляем продукты в корзину
+        User user1 = new User("user1", "password1");
+        user1.addToBasket(product1);
+
+        User user2 = new User("user2", "password2");
+        user2.addToBasket(product2);
+        user2.addToBasket(product3);
+
+        // Выводим на консоль покупки посетителей магазина
+        System.out.println("Покупки посетителей магазина:");
+        System.out.println(user1.getUsername() + " - " + user1.getBasket().getProducts());
+        System.out.println(user2.getUsername() + " - " + user2.getBasket().getProducts());
+    }
 }
--- Produkt.java
+++ Produkt.java
@@ -1,3 +1,105 @@
-public class Produkt {
-    
+import java.util.ArrayList;
+
+class Product {
+    private String name;
+    private double price;
+    private int rating;
+
+    public Product(String name, double price, int rating) {
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{name='" + name + "', price=" + price + ", rating=" + rating + "}";
+    }
+}
+
+class ProductCategory {
+    private String name;
+    private ArrayList<Product> products;
+
+    public ProductCategory(String name) {
+        this.name = name;
+        this.products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductCategory{name='" + name + "', products=" + products + "}";
+    }
+}
+
+class Basket {
+    private ArrayList<Product> products;
+
+    public Basket() {
+        this.products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    @Override
+    public String toString() {
+        return "Basket{products=" + products + "}";
+    }
+}
+
+class User {
+    private String username;
+    private Basket basket;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.basket = new Basket();
+    }
+
+    public void addToBasket(Product product) {
+        this.basket.addProduct(product);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Basket getBasket() {
+        return basket;
+    }
+
+    @Override
+    public String toString() {
+        return "User{username='" + username + "', basket=" + basket + "}";
+    }
 }
